de8bf55ab0d7b5688eb0be52d32d7208c0d8a02c
Mayvi <mayvivek059@gmail.com>
2026-06-01 12:47:20 +0530

    Updated code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,55 @@
+"use client";
+import { useState } from "react";
 import { Home, BookOpen, Settings } from "lucide-react";
 
 export default function Sidebar() {
+
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <aside className="w-64 h-screen bg-zinc-950 text-white p-6 flex flex-col gap-6">
-      
+    <aside
+  className={`h-screen bg-zinc-950 text-white p-6
+  ${collapsed ? "w-20" : "w-64"}`}
+>
       {/* Logo / Brand */}
       <div className="text-xl font-bold">
-       Learning-Dashboard
-      </div>
+  {collapsed ? "LD" : "Learning-Dashboard"}
+</div>
+
+      <button
+  onClick={() => setCollapsed(!collapsed)}
+>
+  Toggle
+</button>
 
       {/* Navigation */}
       <nav className="flex flex-col gap-4 mt-6">
 
-        <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-zinc-800 cursor-pointer">
-          <Home size={18} />
-          <span>Dashboard</span>
-        </div>
+        <div className={`flex items-center p-2 rounded-lg hover:bg-zinc-800
+${collapsed ? "justify-center" : "gap-3"}`}>
+  <Home size={18} />
 
-        <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-zinc-800 cursor-pointer">
-          <BookOpen size={18} />
-          <span>Courses</span>
-        </div>
+  {!collapsed && (
+    <span>Dashboard</span>
+  )}
+</div>
 
-        <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-zinc-800 cursor-pointer">
-          <Settings size={18} />
-          <span>Settings</span>
-        </div>
+        <div className={`flex items-center p-2 rounded-lg hover:bg-zinc-800
+${collapsed ? "justify-center" : "gap-3"}`}>
+  <Home size={18} />
+
+  {!collapsed && (
+    <span>Courses</span>
+  )}
+</div>
+
+        <div className={`flex items-center p-2 rounded-lg hover:bg-zinc-800
+${collapsed ? "justify-center" : "gap-3"}`}>
+  <Home size={18} />
+
+  {!collapsed && (
+    <span>Settings</span>
+  )}
+</div>
 
       </nav>
 
